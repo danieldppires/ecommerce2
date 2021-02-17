4f62ea6e5ca639924359677841172cdcbcc919c0
Daniel Pires <danieldppires@gmail.com>
2021-02-17 14:26:42 -0300

    Site - Produtos

--- site.php
+++ site.php
@@ -1,12 +1,19 @@
 <?php
 
-	use \Hcode\PageAdmin;
+	use \Hcode\Page;
+	use \Hcode\Model\Product;
+	use \Hcode\Model\Category;
+	use \Hcode\Model\User;
 
 	$app->get('/', function() 
 	{
+		$products = Product::listAll();
+
 		$page = new Page();
 
-		$page->setTpl("index");
+		$page->setTpl("index", [
+			'products'=>Product::checkList($products)
+		]);
 	});
 
 ?>
