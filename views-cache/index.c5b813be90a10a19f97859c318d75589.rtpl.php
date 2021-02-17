6f271f643ec928e5cd4e8d1c625f712c513073b9
Daniel Pires <danieldppires@gmail.com>
2021-02-17 18:04:48 -0300

    Categorias VS Produtos

--- views-cache/index.c5b813be90a10a19f97859c318d75589.rtpl.php
+++ views-cache/index.c5b813be90a10a19f97859c318d75589.rtpl.php
@@ -91,7 +91,7 @@
                                     <img src="<?php echo htmlspecialchars( $value1["desphoto"], ENT_COMPAT, 'UTF-8', FALSE ); ?>" alt="">
                                     <div class="product-hover">
                                         <a href="#" class="add-to-cart-link"><i class="fa fa-shopping-cart"></i> Comprar</a>
-                                        <a href="/products/{value.desurl}" class="view-details-link"><i class="fa fa-link"></i> Ver Detalhes</a>
+                                        <a href="/products/<?php echo htmlspecialchars( $value1["desurl"], ENT_COMPAT, 'UTF-8', FALSE ); ?>" class="view-details-link"><i class="fa fa-link"></i> Ver Detalhes</a>
                                     </div>
                                 </div>
                                 
