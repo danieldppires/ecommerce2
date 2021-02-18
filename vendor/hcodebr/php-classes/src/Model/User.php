76d9ec91547ce379f9126081257343e0f5fd72e7
Daniel Pires <danieldppires@gmail.com>
2021-02-18 18:11:11 -0300

    Site - Cadastro Cliente

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -304,5 +304,35 @@
 		{
 			$_SESSION[User::ERROR] = NULL;
 		}
+
+		public static function setErrorRegister($msg)
+		{
+			$_SESSION[User::ERROR_REGISTER] = $msg;
+		}
+
+		public static function getErrorRegister()
+		{
+			$msg = (isset($_SESSION[User::ERROR_REGISTER]) && $_SESSION[User::ERROR_REGISTER]) ? $_SESSION[User::ERROR_REGISTER] : '';
+
+			User::clearErrorRegister();
+
+			return $msg;
+		}
+
+		public static function clearErrorRegister()
+		{
+			$_SESSION[User::ERROR_REGISTER] = NULL;
+		}
+
+		public static function checkLoginExists($login)
+		{
+			$sql = new Sql();
+
+			$results = $sql->select("SELECT * FROM tb_users WHERE deslogin = :deslogin", [
+				':deslogin'=>$login
+			]);
+
+			return (count($results) > 0);
+		}
 	}
 ?>
